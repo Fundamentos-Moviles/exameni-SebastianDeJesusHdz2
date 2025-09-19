5c8f47aca2aaa2faadc9622e716cd4f585569f71
SebastianDeJesusHdz2 <sebastian.hdz2222@gmail.com>
2025-09-18 22:10:43 -0600

    Base

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,30 @@
 import 'package:flutter/material.dart';
 
 const List<Color> colores24 = <Color>[
-  Color(0xFFF44336), // Red 500
-  Color(0xFFE91E63), // Pink 500
-  Color(0xFF9C27B0), // Purple 500
-  Color(0xFF673AB7), // Deep Purple 500
-  Color(0xFF3F51B5), // Indigo 500
-  Color(0xFF2196F3), // Blue 500
-  Color(0xFF03A9F4), // Light Blue 500
-  Color(0xFF00BCD4), // Cyan 500
-  Color(0xFF009688), // Teal 500
-  Color(0xFF4CAF50), // Green 500
-  Color(0xFF8BC34A), // Light Green 500
-  Color(0xFFCDDC39), // Lime 500
-  Color(0xFFFFEB3B), // Yellow 500
-  Color(0xFFFFC107), // Amber 500
-  Color(0xFFFF9800), // Orange 500
-  Color(0xFFFF5722), // Deep Orange 500
-  Color(0xFF795548), // Brown 500
-  Color(0xFF9E9E9E), // Grey 500
-  Color(0xFF607D8B), // Blue Grey 500
-  Color(0xFFD32F2F), // Red 700
-  Color(0xFF7B1FA2), // Purple 700
-  Color(0xFF512DA8), // Deep Purple 700
-  Color(0xFF1976D2), // Blue 700
-  Color(0xFF388E3C), // Green 700
+  Color(0xFFF44336),
+  Color(0xFFE91E63),
+  Color(0xFF9C27B0),
+  Color(0xFF673AB7),
+  Color(0xFF3F51B5),
+  Color(0xFF2196F3),
+  Color(0xFF03A9F4),
+  Color(0xFF00BCD4),
+  Color(0xFF009688),
+  Color(0xFF4CAF50),
+  Color(0xFF8BC34A),
+  Color(0xFFCDDC39),
+  Color(0xFFFFEB3B),
+  Color(0xFFFFC107),
+  Color(0xFFFF9800),
+  Color(0xFFFF5722),
+  Color(0xFF795548),
+  Color(0xFF9E9E9E),
+  Color(0xFF607D8B),
+  Color(0xFFD32F2F),
+  Color(0xFF7B1FA2),
+  Color(0xFF512DA8),
+  Color(0xFF1976D2),
+  Color(0xFF388E3C),
 ];
 
 void main() {
